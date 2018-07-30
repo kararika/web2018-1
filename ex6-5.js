67169575312faf73b0c3094e05ad6396e1e00bce
zm17510 <zm17510@josai.ac.jp>
2018-07-29 20:55:51 -0400

    message

---
+++ ex6-5.js
@@ -0,0 +1,20 @@
+//var who ='world';
+//function sayhello(){
+  //alert('Hello,' + who + '!');
+//}
+//function taro(){
+  //who ='太郎';
+//}
+//function hanako(){
+  //who ='花子';
+//}
+function sayhello(){
+  var who ='world';
+  alert('Hello,'+who+'!');
+}
+function taro(){
+  who ='太郎';
+}
+function hanako(){
+  who ='花子';
+}
